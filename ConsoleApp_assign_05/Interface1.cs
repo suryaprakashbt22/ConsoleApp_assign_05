6921486022d3ebdce51e673e636cf67c6f14059c
Surya Prakash <suryaprakashbt1822@gmail.com>
2022-04-18 20:19:23 +0530

    Add project files.

---
+++ ConsoleApp_assign_05/Interface1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Assign_03_hierarchy
+{
+    interface IPrintable
+    {
+        void printDetails();
+    }
+    public class Employee : IPrintable
+    {
+        string Name, designation;
+        protected double netSalary, grossSalary;
+
+        protected Employee()
+        {
+            this.Name = "";
+            this.designation = "";
+            this.netSalary = 0.0;
+            this.grossSalary = 0.0;
+        }
+
+        protected Employee(string name, string designation, double salary)
+        {
+            this.Name = name;
+            this.designation = designation;
+            this.netSalary = salary;
+        }
+
+        public virtual void CalculateSalary()
+        { 
+            Console.WriteLine("Calculating Salary"); //It will be overrided!
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine("\n---------Details of the Employee---------\n");
+            Console.WriteLine("Name: {0}", this.Name);
+            Console.WriteLine("Designation: {0}", this.designation);
+            Console.WriteLine("Net Salary: {0}", this.netSalary);
+            Console.WriteLine("Gross Salary: {0}\n", this.grossSalary);
+        }
+    }
+
+    public class Manager : Employee
+    {
+        private double petrolAllowance, foodAllowance, otherAllowance;
+
+        public Manager(string name, string designation, double salary) : base(name, designation, salary)
+        {
+            this.petrolAllowance = 0.08 * salary;
+            this.foodAllowance = 0.13 * salary;
+            this.otherAllowance = 0.03 * salary;
+        }
+
+        public override void CalculateSalary()//Overriding
+        {
+            grossSalary = petrolAllowance + foodAllowance + otherAllowance + netSalary;
+
+        }
+    }
+
+    public class MarketingExecutive : Employee
+    {
+        double kilometerTravel, tourAllowance;
+        double telephoneAllowance;
+
+        public MarketingExecutive(string name, string designation, double salary, double kilometers) : base(name, designation, salary)
+        {
+            this.kilometerTravel = kilometers;
+            tourAllowance = 5 * this.kilometerTravel;
+            telephoneAllowance = 1000;
+        }
+
+        public override void CalculateSalary()
+        {
+            grossSalary = netSalary + kilometerTravel + tourAllowance + telephoneAllowance;
+        }
+    }
+}
---
+++ ConsoleApp_assign_05/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+using Empclass = ConsoleApp_employeeclass_for_arraylist;
+
+namespace ConsoleApp_assign_05
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int N=int.Parse(Console.ReadLine());
+            int[] arr1 = new int[N];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = int.Parse(Console.ReadLine());
+            }
+            int[] arr2=new int[N];
+            Array.Copy(arr1, 0, arr2, 0, arr2.Length);
+            Console.WriteLine("Elements copied in the new array::");
+            foreach (int i in arr2)
+            {
+                Console.WriteLine(i);
+            }
+            Array.Sort(arr1);
+            Console.WriteLine("First Array after sorting::");
+            foreach(int i in arr1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("After Reversing Array 01::");
+            Array.Reverse(arr1);
+            foreach(int i in arr1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Now clearing the Array from first to last index::");
+            Array.Clear(arr1,0,arr1.Length);
+            foreach(int i in arr1)
+            {
+                Console.WriteLine(i);
+            }
+
+            var arrayList1 = new ArrayList();
+            Empclass.Manager m1 = new Empclass.Manager("SP", "Manager", 1000);
+            Empclass.MarketingExecutive m2 = new Empclass.MarketingExecutive("B", "Marketing Executive", 1000000.0, 10);
+            Empclass.Manager m3 = new Empclass.Manager("C", "Manager", 1000000.0);
+
+            arrayList1.Add(m1);
+            arrayList1.Add(m2);
+            arrayList1.Add(m3);
+
+            Console.WriteLine("Printing the Array List!");
+            
+            foreach (var element in arrayList1)
+            {
+                Empclass.Employee temp = (Empclass.Employee)element;
+                temp.CalculateSalary();
+                temp.printDetails();
+                Console.WriteLine("Detail of the type of the object stored ::{0}",element);
+                Console.WriteLine();
+            }
+            //Console.ReadLine();
+
+            Console.WriteLine("Implementing Linked List Now :: ");
+
+            LinkedList<Empclass.Employee> emplinkedlist= new LinkedList<Empclass.Employee>();
+            emplinkedlist.AddLast(m1);
+            emplinkedlist.AddLast(m2);
+            emplinkedlist.AddLast(m3);
+            Empclass.Manager m4 = new Empclass.Manager("D", "Manager", 15000);
+            Console.WriteLine("\nAdding an element to the front of the Linked List!\n");
+            emplinkedlist.AddFirst(m4);
+            int cntOfEmployeesInLinkedlist = 0;
+            foreach(var element in emplinkedlist)
+            {
+                Empclass.Employee temp = (Empclass.Employee)element;
+                temp.CalculateSalary();
+                temp.printDetails();
+                cntOfEmployeesInLinkedlist++;
+                Console.WriteLine("Detail of the type of the object stored ::{0}", element);
+                Console.WriteLine();
+            }
+            Console.WriteLine("No. of Elements in Linked List = {0}", cntOfEmployeesInLinkedlist);
+            Console.ReadLine();
+            
+        }
+    }
+}
